0742ddf63d8b92d1716b820d5ebb3ef826c98849
Jacek Konieczny <jajcus@jajcus.net>
2004-06-01 19:00:25 +0000

    - profile option handling fixed again

--- jjigw.py
+++ jjigw.py
@@ -44,9 +44,9 @@ def main(profile=False):
         print "Aborting."
         sys.exit(1)
 
-if sys.argv[1]=='--profile':
+if '--profile' in sys.argv:
     import profile
-    sys.argv[1:]=sys.argv[2:]
+    sys.argv.remove('--profile')
     profile.run("main(profile=True)","jjigw.prof")
 else:
     main()
